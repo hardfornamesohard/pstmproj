c6ca501c369ec097b32bddad780b49c41d337cc8
hardfornamesohard <1037456450@qq.com>
2022-11-23 10:24:10 +0800

    finish private Agent[] hasAugmentationCycle() _1. _2

--- pstmproj/src/pstmproj/PSTM.java
+++ pstmproj/src/pstmproj/PSTM.java
@@ -7,10 +7,13 @@ public class PSTM {
 	private Agent[] courses;
 	//当前的匹配对集合，key存course，value存course-student
 	private SymbolTable<Agent, Agent[]> matches;
+	//检查cycle时使用，索引处的值对应同索引students数组中的参与人是否匹配
+	private boolean[] marked;
 	public PSTM() {
 		initAgents();
 		initPreference();
 		matches = new SymbolTable<Agent, Agent[]>();
+		marked = new boolean[students.length];
 	}
 	public void initAgents() {
 		students = new Agent[4];
@@ -96,7 +99,48 @@ public class PSTM {
 	}
 	//augmentationcycle是已匹配的参与人集合，若不存在则返回null,
 	private Agent[] hasAugmentationCycle() {
+		//获取已匹配参与人集合，偶数下标存放student，奇数下标存放course
+		//得到已匹配的参与人数量，构建cycle
+		int number = 0;
+		for(Agent key : courses) {
+			if(matches.get(key) == null) continue;
+			number += matches.get(key).length;
+		}
+		if((number == 0) | (number % 2 != 0)) return null;
+		//将已匹配的参与人按cycle排列
+		Agent[] cycle = new Agent[number];
+		int cindex = 1;
+		for(Agent course : courses) {
+			//course已匹配才加入cycle
+			if(matches.get(course) != null) {
+				cycle[cindex] = course;
+				for(Agent agent : matches.get(course)) {
+					int index = this.indexOf(agent, students);
+					//跳过value数组的索引0处的course
+					if(index > -1) marked[index] = true;
+					else continue;
+				}
+				cindex += 2;
+			}
+		}
+		int sindex = 0;
+		for(Agent student : students) {
+			int index = this.indexOf(student, students);
+			if(marked[index] == true && sindex < cycle.length) {
+				cycle[sindex] = student;
+				sindex += 2;
+			}
+		}
+		//判断是否同下标匹配
+		
+		//是否存在一个严格偏好
 		return null;
+	}
+	private int indexOf(Agent agent, Agent[] agents) {
+		for(int i = 0; i < agents.length; i++) {
+			if(agent == agents[i]) return i;
+		}
+		return -1;
 	}
 	private void eliminatePath(Agent[] path) {
 		
